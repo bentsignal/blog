095222e6489644bb91941fe5e720da192cbcbfc3
Shawn Rodgers <me@bentsignal.com>
2025-09-20 16:17:27 -0400

    add link to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
+"use client";
+
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { SelectorComposer, StandardComposer } from "@/components/composers";
+import { SimpleIcons } from "@/components/simple-icons";
+import { Button } from "@/components/ui/button";
 import * as Card from "@/components/ui/card";
 
 export default function Home() {
+  const { theme } = useTheme();
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <Link
+        href="https://github.com/bentsignal/composition-is-all-you-need"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button variant="outline">
+          Code
+          <SimpleIcons
+            icon="siGithub"
+            color={theme === "dark" ? "white" : "black"}
+          />
+        </Button>
+      </Link>
       <div className="flex items-center justify-center gap-4">
         <Card.Card>
           <Card.CardHeader>
-            <Card.CardTitle className="text-green-300">
+            <Card.CardTitle className="text-good">
               With context selector
             </Card.CardTitle>
             <Card.CardDescription>
               This version uses{" "}
               <Link
-                className="text-blue-300"
+                className="text-link"
                 href="https://www.npmjs.com/package/@fluentui/react-context-selector"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -30,7 +50,7 @@ export default function Home() {
         </Card.Card>
         <Card.Card>
           <Card.CardHeader>
-            <Card.CardTitle className="text-red-300">
+            <Card.CardTitle className="text-bad">
               Without context selector
             </Card.CardTitle>
             <Card.CardDescription>
