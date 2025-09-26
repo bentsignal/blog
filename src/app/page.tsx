b8e6816f6c9b3281bf0d8c9e476d450249d495ae
Shawn Rodgers <me@bentsignal.com>
2025-09-26 15:26:56 -0400

    phone layout looks right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,22 @@
-import Link from "next/link";
 import { MainComposer } from "@/components/composers";
-import { RepoButton } from "@/components/repo-button";
 import * as Card from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <Link
-        href="https://github.com/bentsignal/composition-is-all-you-need"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <RepoButton />
-      </Link>
-      <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
-        <Card.Card className="rounded-3xl p-2">
-          <Card.CardContent className="p-2">
-            <div className="align-start flex max-h-[500px] flex-col justify-start gap-2 overflow-y-auto bg-blue-500">
-              {Array.from({ length: 100 }).map((_, index) => (
-                <div
-                  key={index}
-                  className="bg-muted size-10 min-h-10 rounded-md"
-                />
-              ))}
-            </div>
-            <MainComposer />
-          </Card.CardContent>
-        </Card.Card>
-      </div>
+    <div className="flex h-screen items-center justify-center py-4">
+      <Card.Card className="h-[700px] max-h-full rounded-3xl p-2">
+        <Card.CardContent className="flex h-full flex-col p-2">
+          <div className="align-start flex min-h-0 flex-1 flex-col justify-start gap-2 overflow-y-auto mask-b-from-80% pb-8">
+            {Array.from({ length: 100 }).map((_, index) => (
+              <div
+                key={index}
+                className="bg-muted size-10 min-h-10 rounded-md"
+              />
+            ))}
+          </div>
+          <MainComposer />
+        </Card.CardContent>
+      </Card.Card>
     </div>
   );
 }
