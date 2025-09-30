84cc7f5a8dca2f81f4a4165400355a740458f17c
Shawn Rodgers <me@bentsignal.com>
2025-09-29 20:14:09 -0400

    remove auth modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import * as Auth from "@/components/auth";
 import { Phone } from "@/components/phone";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { getToken } from "@/lib/auth-server";
@@ -8,7 +7,6 @@ export default async function Home() {
   const authed = token !== undefined;
   return (
     <div className="flex h-screen flex-col items-center justify-center overflow-y-auto py-4">
-      <Auth.Modal />
       <div className="fixed top-4 right-4 z-50">
         <ThemeToggle />
       </div>
--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { XIcon } from "lucide-react";
-import { create } from "zustand";
 import { Spinner } from "./spinner";
 import { Button } from "./ui/button";
-import * as Dialog from "@/components/ui/dialog";
 import { authClient } from "@/lib/auth-client";
 
 export const Profile = () => {
@@ -36,51 +33,5 @@ export const SignIn = () => {
     >
       {loading ? <Spinner /> : "Join the conversation"}
     </Button>
-  );
-};
-
-const useAuthModal = create<{
-  open: boolean;
-  setOpen: (open: boolean) => void;
-}>((set) => ({
-  open: false,
-  setOpen: (open) => set({ open }),
-}));
-
-export const Modal = () => {
-  const { open, setOpen } = useAuthModal();
-  const [loading, setLoading] = useState(false);
-
-  return (
-    <Dialog.Dialog open={open} onOpenChange={setOpen}>
-      <Dialog.DialogContent
-        showCloseButton={false}
-        className="reltative flex items-center justify-center focus:outline-none"
-      >
-        <Dialog.DialogHeader className="sr-only">
-          <Dialog.DialogTitle>Sign in</Dialog.DialogTitle>
-        </Dialog.DialogHeader>
-        <span>Sign in to send messages</span>
-        <Button
-          onClick={async () => {
-            try {
-              setLoading(true);
-              await authClient.signIn.social({ provider: "github" });
-            } catch (error) {
-              console.error(error);
-            } finally {
-              setLoading(false);
-            }
-          }}
-        >
-          {loading ? <Spinner /> : "Sign in with Github"}
-        </Button>
-        <div className="absolute top-0 right-2">
-          <Button variant="ghost" size="icon" onClick={() => setOpen(false)}>
-            <XIcon />
-          </Button>
-        </div>
-      </Dialog.DialogContent>
-    </Dialog.Dialog>
   );
 };
