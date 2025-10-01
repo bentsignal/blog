f4f56a67e6763ca1623b53bac63e7a0263a3f516
Shawn Rodgers <me@bentsignal.com>
2025-10-01 15:29:40 -0400

    don't hide page loader until mount

--- src/components/repo-button.tsx
+++ src/components/repo-button.tsx
@@ -1,31 +1,20 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { SimpleIcons } from "./simple-icons";
 import { Button } from "./ui/button";
 
 export const RepoButton = () => {
-  const [mounted, setMounted] = useState(false);
   const { resolvedTheme } = useTheme();
 
-  useEffect(() => setMounted(true), []);
-
   return (
-    <div
-      className="transition-opacity duration-300"
-      style={{
-        opacity: mounted ? 1 : 0,
-      }}
-    >
+    <div className="transition-opacity duration-300">
       <Button variant="outline">
         Code
-        {mounted && (
-          <SimpleIcons
-            icon="siGithub"
-            color={resolvedTheme === "dark" ? "white" : "black"}
-          />
-        )}
+        <SimpleIcons
+          icon="siGithub"
+          color={resolvedTheme === "light" ? "black" : "white"}
+        />
       </Button>
     </div>
   );
