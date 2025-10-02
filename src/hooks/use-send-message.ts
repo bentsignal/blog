039d8a856adc7ebd14fd384a0f957ac1bc233e58
Shawn Rodgers <me@bentsignal.com>
2025-10-02 16:47:23 -0400

    toast when a message fails to send

--- src/hooks/use-send-message.ts
+++ src/hooks/use-send-message.ts
@@ -1,40 +1,51 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useMutation } from "convex/react";
+import { useConvexMutation } from "@convex-dev/react-query";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 import { useAuth } from "@/components/auth";
 
 export const useSendMessage = () => {
   const image = useAuth((c) => c.image);
   const name = useAuth((c) => c.name);
 
-  const sendMessage = useMutation(
-    api.messages.sendMessage,
-  ).withOptimisticUpdate((localStore, args) => {
-    const results = localStore.getAllQueries(api.messages.getMessages);
-    const current = results[0];
-    if (!current || !current.value) return;
-    localStore.setQuery(
-      api.messages.getMessages,
-      {
-        paginationOpts: current.args.paginationOpts,
-      },
-      {
-        ...current.value,
-        page: [
-          {
-            name: name || "",
-            pfp: image,
-            _id: ("optimistic-" +
-              Math.random().toString(36).slice(2)) as Id<"messages">,
-            _creationTime: Date.now(),
-            content: args.content,
-          },
-          ...(current.value.page || []),
-        ],
-        isDone: current.value.isDone ?? false,
-        continueCursor: current.value.continueCursor ?? "",
-      },
-    );
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: useConvexMutation(
+      api.messages.sendMessage,
+    ).withOptimisticUpdate((localStore, args) => {
+      const results = localStore.getAllQueries(api.messages.getMessages);
+      const current = results[0];
+      if (!current || !current.value) return;
+      localStore.setQuery(
+        api.messages.getMessages,
+        {
+          paginationOpts: current.args.paginationOpts,
+        },
+        {
+          ...current.value,
+          page: [
+            {
+              name: name || "",
+              pfp: image,
+              _id: ("optimistic-" +
+                Math.random().toString(36).slice(2)) as Id<"messages">,
+              _creationTime: Date.now(),
+              content: args.content,
+            },
+            ...(current.value.page || []),
+          ],
+          isDone: current.value.isDone ?? false,
+          continueCursor: current.value.continueCursor ?? "",
+        },
+      );
+    }),
+    onError: (error) => {
+      console.error(error);
+      console.log(error.message);
+      toast.error("Failed to send message", {
+        description: "Something went wrong, see console for more details.",
+      });
+    },
   });
 
   return {
