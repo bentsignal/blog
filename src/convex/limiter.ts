b97dcb63204c48ffa1ac92550dae23588169058e
Shawn Rodgers <me@bentsignal.com>
2025-10-03 11:53:15 -0400

    update rate limit for sending messages

--- src/convex/limiter.ts
+++ src/convex/limiter.ts
@@ -2,5 +2,5 @@ import { MINUTE, RateLimiter } from "@convex-dev/rate-limiter";
 import { components } from "./_generated/api";
 
 export const rateLimiter = new RateLimiter(components.rateLimiter, {
-  sendMessage: { kind: "token bucket", rate: 60, period: MINUTE, capacity: 5 },
+  sendMessage: { kind: "token bucket", rate: 30, period: MINUTE, capacity: 10 },
 });
