ebe84ae8dfb55de4ac199f829a55269a99abaafe
Shawn Rodgers <me@bentsignal.com>
2025-10-11 12:36:18 -0400

    add some padding to the text area

--- src/components/composers/composer.tsx
+++ src/components/composers/composer.tsx
@@ -139,7 +139,7 @@ export const Input = ({
       ref={inputRef}
       rows={1}
       className={cn(
-        "mr-3 flex w-full min-w-0 flex-1",
+        "mr-3 flex w-full min-w-0 flex-1 p-1",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         "resize-none select-none",
         "scrollbar-track-transparent scrollbar-thin scrollbar-thumb-card overflow-y-auto",
