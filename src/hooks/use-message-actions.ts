69732c4d84b659db84c122092ab82759137c21e2
Shawn Rodgers <me@bentsignal.com>
2025-10-16 15:33:15 -0400

    use channel from mutation args for send optimistic update

--- src/hooks/use-message-actions.ts
+++ src/hooks/use-message-actions.ts
@@ -46,7 +46,7 @@ export const useMessageActions = () => {
             Math.random().toString(36).slice(2)) as Id<"messages">,
           _creationTime: Date.now(),
           profile: myProfileId as Id<"profiles">,
-          channel: "optimistic-channel" as Id<"channels">,
+          channel: args.channel,
           snapshots: [
             {
               content: args.content,
