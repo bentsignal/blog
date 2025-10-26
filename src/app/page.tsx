6b3862cbc6fb6e93fad8ff8c234a4c8eb02c44cb
Shawn Rodgers <me@bentsignal.com>
2025-10-26 18:58:07 -0400

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,11 @@ import * as Card from "@/ui/atoms/card";
 export default async function HomePage() {
   const posts = await fetchQuery(api.posts.getAll, {});
   return (
-    <div className="mx-auto flex h-screen max-w-xl flex-col items-center justify-center gap-2">
-      HomePage
+    <div className="mx-auto flex h-screen max-w-xl flex-col justify-center gap-4">
       {posts.map((post) => (
         <Link href={`/${post.slug}`} key={post._id}>
           <Card.Frame className="px-0 py-4">
-            <Card.Content>
+            <Card.Content className="flex flex-col gap-1">
               <Card.Title>{post.title}</Card.Title>
               <Card.Description>{post.subtitle}</Card.Description>
             </Card.Content>
