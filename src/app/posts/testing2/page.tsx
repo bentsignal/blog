320ea7f2bf11d7825beb7b72b8f63725e858e20e
Shawn Rodgers <me@bentsignal.com>
2025-11-02 15:10:14 -0500

    cache page test

---
+++ src/app/posts/testing2/page.tsx
@@ -0,0 +1,9 @@
+"use cache";
+
+export default async function Testing2Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      Testing2Page
+    </div>
+  );
+}
