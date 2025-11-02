d5cd5ed12862f111a62b416c1850db83acda158b
Shawn Rodgers <me@bentsignal.com>
2025-11-02 13:26:14 -0500

    cache home page

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  cacheComponents: true,
   images: {
     remotePatterns: [
       {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+"use cache";
+
 import { posts, postSlugs } from "@/data/posts";
 import PostCard from "@/ui/molecules/post-card";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div className="mx-auto flex h-screen max-w-xl flex-col items-center justify-center gap-4">
       {postSlugs.map((slug) => {
