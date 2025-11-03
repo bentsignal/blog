008ba4a1ae5ee9ad578acf731b252ff18767d5bd
Shawn Rodgers <me@bentsignal.com>
2025-11-03 16:05:29 -0500

    pass mouse events through abyss

--- src/ui/atoms/abyss.tsx
+++ src/ui/atoms/abyss.tsx
@@ -1,16 +1,12 @@
 import { cn } from "@/utils/style-utils";
 
 const shared =
-  "bg-background/10 absolute z-5 flex h-16 w-full backdrop-blur-sm";
+  "bg-background/10 absolute z-5 left-0 flex h-16 w-full backdrop-blur-sm pointer-events-none";
 
 export const Top = ({ className }: { className?: string }) => {
-  return (
-    <div className={cn(shared, "top-0 left-0 mask-b-from-35%", className)} />
-  );
+  return <div className={cn(shared, "top-0 mask-b-from-35%", className)} />;
 };
 
 export const Bottom = ({ className }: { className?: string }) => {
-  return (
-    <div className={cn(shared, "bottom-0 left-0 mask-t-from-35%", className)} />
-  );
+  return <div className={cn(shared, "bottom-0 mask-t-from-35%", className)} />;
 };
