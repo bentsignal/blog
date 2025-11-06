71a16d580280adc50a8d18f66bc146365cbd0868
Shawn Rodgers <me@bentsignal.com>
2025-11-06 12:43:14 -0500

    center message on not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-1 px-4">
       <h1 className="text-4xl font-bold">Oops</h1>
-      <p className="text-muted-foreground mb-1 text-lg">
+      <p className="text-muted-foreground mb-1 text-center text-lg">
         We couldn't find the page you were looking for. Sorry about that.
       </p>
       <Link href="/">
