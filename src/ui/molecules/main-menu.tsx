1e45274d8861e114c2afdccbe572738e42c8c5ed
Shawn Rodgers <me@bentsignal.com>
2025-11-22 15:43:17 -0500

    main menu opens by click instead of hover

--- src/ui/molecules/main-menu.tsx
+++ src/ui/molecules/main-menu.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import * as Auth from "@/ui/atoms/auth";
 import * as Popover from "@/ui/atoms/popover";
 import { Separator } from "@/ui/atoms/separator";
@@ -7,18 +7,17 @@ import { ThemeToggle } from "@/ui/molecules/theme-toggle";
 
 export default function MainMenu() {
   const [open, setOpen] = useState(false);
-
   return (
-    <div
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-      className="flex cursor-pointer items-center"
-    >
+    <div className="flex cursor-pointer items-center">
       <Popover.Frame open={open} onOpenChange={setOpen}>
-        <Popover.Trigger className="cursor-pointer p-2 pl-8 outline-none!">
-          <Menu className="size-4.5 cursor-pointer" />
+        <Popover.Trigger className="cursor-pointer p-3 pl-8 outline-none!">
+          {open ? (
+            <X className="size-4.5 cursor-pointer" />
+          ) : (
+            <Menu className="size-4.5 cursor-pointer" />
+          )}
         </Popover.Trigger>
-        <Popover.Content className="-mt-1 mr-4 flex w-auto min-w-32 flex-col items-start p-1 sm:min-w-52">
+        <Popover.Content className="-mt-2 mr-4 flex w-auto min-w-32 flex-col items-start p-1 sm:min-w-52">
           <Auth.PrimaryButton />
           {/* <Auth.DeleteAccountButton /> */}
           <Separator className="my-1" />
