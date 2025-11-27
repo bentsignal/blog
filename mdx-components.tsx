6b548c379be580cc1d0f80cb9366d51afa85802e
Shawn Rodgers <me@bentsignal.com>
2025-11-27 13:30:52 -0500

    use code atom in mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,29 @@
 import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
+import * as Code from "@/atoms/code";
 
-const components: MDXComponents = {};
+const components: MDXComponents = {
+  code: Code.Provider,
+  pre: ({ children }) => <>{children}</>,
+  a: ({ href, children, ...props }) => (
+    // eslint-disable-next-line
+    <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+    </a>
+  ),
+  img: ({ src, alt, ...props }) => {
+    return (
+      <Image
+        src={src}
+        alt={alt}
+        width={800}
+        height={600}
+        className="rounded-xl"
+        {...props}
+      />
+    );
+  },
+};
 
 export function useMDXComponents(): MDXComponents {
   return components;
