870df7624619967ecda214efbcc96caa528334ab
Shawn Rodgers <me@bentsignal.com>
2025-11-28 14:02:17 -0500

    upgrade abyss

--- src/atoms/abyss.tsx
+++ src/atoms/abyss.tsx
@@ -1,12 +1,84 @@
 import { cn } from "@/utils/style-utils";
 
-const shared =
-  "bg-background/10 absolute z-5 left-0 flex h-16 w-full backdrop-blur-sm pointer-events-none";
+const shared = "bg-background/10 absolute z-5 flex pointer-events-none";
 
-export const Top = ({ className }: { className?: string }) => {
-  return <div className={cn(shared, "top-0 mask-b-from-35%", className)} />;
+const verticalShared = "left-0 w-full h-16";
+const horizontalShared = "top-0 h-full w-16";
+
+type BlurSize = "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+
+const getBlurSize = (blurSize: BlurSize) =>
+  blurSize === "xs"
+    ? "backdrop-blur-xs"
+    : blurSize === "sm"
+      ? "backdrop-blur-sm"
+      : blurSize === "md"
+        ? "backdrop-blur-md"
+        : blurSize === "lg"
+          ? "backdrop-blur-lg"
+          : blurSize === "xl"
+            ? "backdrop-blur-xl"
+            : blurSize === "2xl"
+              ? "backdrop-blur-2xl"
+              : "backdrop-blur-3xl";
+
+interface AbyssProps {
+  className?: string;
+  blurSize?: BlurSize;
+}
+
+export const Top = ({ className, blurSize = "sm" }: AbyssProps) => {
+  return (
+    <div
+      className={cn(
+        className,
+        shared,
+        verticalShared,
+        "top-0 mask-b-from-35%",
+        getBlurSize(blurSize),
+      )}
+    />
+  );
 };
 
-export const Bottom = ({ className }: { className?: string }) => {
-  return <div className={cn(shared, "bottom-0 mask-t-from-35%", className)} />;
+export const Bottom = ({ className, blurSize = "sm" }: AbyssProps) => {
+  return (
+    <div
+      className={cn(
+        className,
+        shared,
+        verticalShared,
+        "bottom-0 mask-t-from-35%",
+        getBlurSize(blurSize),
+      )}
+    />
+  );
+};
+
+export const Left = ({ className, blurSize = "sm" }: AbyssProps) => {
+  return (
+    <div
+      className={cn(
+        className,
+        shared,
+        horizontalShared,
+        "left-0 mask-r-from-35%",
+        getBlurSize(blurSize),
+      )}
+    />
+  );
+};
+
+export const Right = ({ className, blurSize = "sm" }: AbyssProps) => {
+  return (
+    <div
+      className={cn(
+        className,
+        shared,
+        horizontalShared,
+        "right-0",
+        getBlurSize(blurSize),
+      )}
+    />
+  );
 };
