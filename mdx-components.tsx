52bb612dd8801884ac388f5a19b7bdf94860f8d7
Shawn Rodgers <me@bentsignal.com>
2025-12-04 13:34:34 -0500

    only open external links in new tab, use link component for internal links

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,16 +1,27 @@
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
+import Link from "next/link";
 import * as Code from "@/atoms/code";
 
 const components: MDXComponents = {
   code: Code.Provider,
   pre: ({ children }) => <>{children}</>,
-  a: ({ href, children, ...props }) => (
-    // eslint-disable-next-line
-    <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
-      {children}
-    </a>
-  ),
+  a: ({ href, children, ...props }) => {
+    const isExternalLink = /^https?:\/\//.test(href);
+    if (isExternalLink) {
+      return (
+        // eslint-disable-next-line
+        <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+          {children}
+        </a>
+      );
+    }
+    return (
+      <Link href={href} {...props}>
+        {children}
+      </Link>
+    );
+  },
   img: ({ src, alt, ...props }) => {
     return (
       <Image
