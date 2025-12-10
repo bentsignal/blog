ee208bb35f0cad409691cfe295a15ddf122fba90
Shawn Rodgers <me@bentsignal.com>
2025-12-09 21:08:16 -0500

    Revert "remove unused not found page"
    This reverts commit 270d5113bc87855db26461dae988527c861c1b53.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,3 +3,17 @@ import HomePage from "./page";
 export default function NotFound() {
   return <HomePage />;
 }
+
+// export default function NotFound() {
+//   return (
+//     <div className="flex h-screen w-full flex-col items-center justify-center gap-1 px-4">
+//       <h1 className="text-4xl font-bold">Oops</h1>
+//       <p className="text-muted-foreground mb-1 text-center text-lg">
+//         We couldn't find the page you were looking for. Sorry about that.
+//       </p>
+//       <Link href="/">
+//         <Button variant="outline">Back to Home</Button>
+//       </Link>
+//     </div>
+//   );
+// }
